cd42c7d73fe65ec2de55bb90353c7f3c2830627c
Judit Jose <juditjose.pdve@gmail.com>
2024-12-17 19:56:09 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++
@@ -1,7 +0,0 @@
-
-export default {
-  basePath: '/army-club/',
-  entryPoints: {
-    '': () => import('./main.server.mjs')
-  },
-};
--- server/assets-chunks/styles-5NTETIBU_css.mjs
+++
@@ -1,2 +0,0 @@
-export default `@import"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
-`;
