e76f109a75f9761358e0f8d7b63acf96e3726fe8
Judit Jose <juditjose.pdve@gmail.com>
2024-12-17 19:34:27 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/army-club/',
   entryPoints: {
     '': () => import('./main.server.mjs')
   },
--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>ArmyClub</title>
-  <base href="/">
+  <base href="/army-club/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <style>@import"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";</style><link rel="stylesheet" href="styles-5NTETIBU.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-5NTETIBU.css"></noscript><link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" as="style"></head>
